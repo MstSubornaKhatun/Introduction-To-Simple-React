bdcef51d4070595b6e2606203b1863d8b8323f7a
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-01-14 15:45:12 +0600

    m1

---
+++ m40/react-world-on-the-go/src/components/Countries/Countries.jsx
@@ -0,0 +1,26 @@
+function Countries() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+// ------------------
+
+const Countries = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+// react extension pack install 
+
+
+/*
+ai vabe duivabe function declare 
+kora jai. arrow function best 
+practice.
+*/
