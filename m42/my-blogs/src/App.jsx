f0b948670574d274ef29c38bff7f809f2fe7904c
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-01-16 17:12:49 +0600

    react

--- m42/my-blogs/src/App.jsx
+++ m42/my-blogs/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -7,6 +8,19 @@ function App() {
   return (
     <>
     <Navbar></Navbar>
+    
+    
+
+    <div className="main-container text-center flex">
+      <div className="left-container w-[70%]">
+       
+       <Blogs></Blogs>
+      </div>
+      <div className="right-container w-[30%]">
+        <h1>Reading time: 0</h1>
+        <h1>Bookmarked count: 0</h1>
+      </div>
+    </div>
      
       
     </>
