2485382ebf5fc70e7e5ed59ee472ab77e93ed798
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-01-17 21:01:42 +0600

    m42

--- m42/my-blogs/src/App.jsx
+++ m42/my-blogs/src/App.jsx
@@ -13,13 +13,7 @@ function App() {
     setBookmarked([...bookmarked, blog])
   }
   console.log(bookmarked)
-/*
-jehetu function er modhe theke jetar bookmark te click korle setar sompurno data ta paachi tai akhn  state declate kore state er modhe rekhe dite pari.  
 
-
-jehetu bookmark a click korle data gulo paschi. sehetu akhn map caliye sei data gulo right-container er modhe dekhabo
-
-*/  
   return (
     <>
     <Navbar></Navbar>
@@ -33,17 +27,21 @@ jehetu bookmark a click korle data gulo paschi. sehetu akhn map caliye sei data 
       </div>
       <div className="right-container w-[30%]">
         <h1>Reading time: 0</h1>
-        <h1>Bookmarked count: 0</h1>
+        <h1>Bookmarked count: {bookmarked.length}</h1>
 
         {
           bookmarked.map((marked)=><p>{marked.title}</p> )
         }
       </div>
     </div>
-     
-      
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
